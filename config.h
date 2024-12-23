c9dcd602cc23041659902027dc89ba30549bc0f9
Ales Komarek <ales-komarek@seznam.cz>
2024-12-23 12:58:00 +0000

    refactor: conf

--- config.h
+++ config.h
@@ -38,15 +38,15 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 
-	/* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",         NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "LibreWolf",    NULL,     NULL,           0,         0,          0,           0,        -1 },
-  { "KeePassXC",	  NULL,		  NULL,       		0,		     0,		       0,           0,        -1 },
-  { "discord",	    NULL,		  NULL,       		1 << 8,		 0,		       0,           0,         0 },
-	{ "st",           NULL,     NULL,           0,         0,          1,           0,        -1 },
-  { "Pentablet",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ NULL, NULL,     "XBindKey: Hit a key",    0,         0,          0,           1,        -1 }, 
+	/* class		instance  	title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",         	NULL,     	NULL,		0,         1,          0,           0,        -1 },
+	{ "LibreWolf",    	NULL,     	NULL,           0,         0,          0,           0,        -1 },
+  	{ "KeePassXC",	  	NULL,		NULL,       	0,	   0,	       0,           0,        -1 },
+  	{ "discord",	  	NULL,		NULL,       	1 << 8,	   0,	       0,           0,         0 },
+	{ "st",           	NULL,     	NULL,           0,         0,          1,           0,        -1 },
+  	{ "Pentablet",    	NULL,     	NULL,           0,         1,          0,           0,        -1 },
+	{ NULL,           	NULL,     	"Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL, NULL,     	"XBindKey: Hit a key",    	0,         0,          0,           1,        -1 }, 
 };
 
 /* layout(s) */
